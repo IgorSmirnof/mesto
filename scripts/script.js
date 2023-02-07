875b70bcb37424e0c078a8b2928a750478c54991
IgorSmirnof <igor.smirna@gmail.com>
2023-02-07 22:04:00 +0300

    send to review

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,35 @@
 // console.log('js must go on');
 
-let buttonEdit = document.querySelector(".profile__button-edit");
-let popup = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileDiscription = document.querySelector(".profile__discription");
-let inputName = document.querySelector(".popup__name");
-let inputDiscription = document.querySelector(".popup__discription");
-let formSend = document.querySelector(".popup__container");
+const popup = document.querySelector(".popup");
+const profile = document.querySelector(".profile");
+const like = document.querySelectorAll(".card__like");
 
-let buttonClose = document.querySelector(".popup__close");
-let buttonSave = document.querySelector(".popup__button-save");
-let like = document.querySelectorAll(".card__like");
-let buttonAdd = document.querySelector(".profile__button-add");
+const profileName = profile.querySelector(".profile__name");
+const profileDescription = profile.querySelector(".profile__description");
+const inputData = popup.querySelectorAll(".popup__input");
+const formSend = popup.querySelector(".popup__container");
+
+const buttonEdit = profile.querySelector(".profile__button-edit");
+const buttonClose = popup.querySelector(".popup__close");
+const buttonSave = popup.querySelector(".popup__button-save");
+const buttonAdd = profile.querySelector(".profile__button-add");
+
+inputData[0].value = profileName.textContent;
+inputData[1].value = profileDescription.textContent;
 
 buttonEdit.addEventListener("click", () => popup.classList.add("popup_opened"));
 buttonClose.addEventListener("click", () =>
   popup.classList.remove("popup_opened")
 );
+
 formSend.addEventListener("submit", function (e) {
   e.preventDefault();
-  profileName.textContent = inputName.value;
-  profileDiscription.textContent = inputDiscription.value;
+  profileName.textContent = inputData[0].value;
+  profileDescription.textContent = inputData[1].value;
   popup.classList.remove("popup_opened");
 });
 
-let elements = document.querySelector(".elements");
-
+const elements = document.querySelector(".elements");
 elements.addEventListener("click", function (event) {
   if (event.target.classList[0] == "card__like") {
     event.target.classList.toggle("card__like_active");
