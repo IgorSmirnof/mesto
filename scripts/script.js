0be59ea844627fc896c6f8a3417786deb946a9c8
IgorSmirnof <igor.smirna@gmail.com>
2023-02-09 19:26:58 +0300

    send to review

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,32 @@
-// console.log('js must go on');
-
 const popup = document.querySelector(".popup");
 const profile = document.querySelector(".profile");
-const like = document.querySelectorAll(".card__like");
+const elements = document.querySelector(".elements");
 
 const profileName = profile.querySelector(".profile__name");
 const profileDescription = profile.querySelector(".profile__description");
-const inputData = popup.querySelectorAll(".popup__input");
-const formSend = popup.querySelector(".popup__container");
-
 const buttonEdit = profile.querySelector(".profile__button-edit");
+const formSend = popup.querySelector(".popup__container");
 const buttonClose = popup.querySelector(".popup__close");
-const buttonSave = popup.querySelector(".popup__button-save");
-const buttonAdd = profile.querySelector(".profile__button-add");
+const inputName = popup.querySelector(".popup__input_name");
+const inputDescription = popup.querySelector(".popup__input_description");
 
-inputData[0].value = profileName.textContent;
-inputData[1].value = profileDescription.textContent;
+buttonEdit.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+});
 
-buttonEdit.addEventListener("click", () => popup.classList.add("popup_opened"));
-buttonClose.addEventListener("click", () =>
-  popup.classList.remove("popup_opened")
-);
+buttonClose.addEventListener("click", () => popup.classList.remove("popup_opened"));
 
 formSend.addEventListener("submit", function (e) {
   e.preventDefault();
-  profileName.textContent = inputData[0].value;
-  profileDescription.textContent = inputData[1].value;
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
   popup.classList.remove("popup_opened");
 });
 
-const elements = document.querySelector(".elements");
 elements.addEventListener("click", function (event) {
-  if (event.target.classList[0] == "card__like") {
+  if (event.target.classList.contains("card__like")) {
     event.target.classList.toggle("card__like_active");
   }
 });
