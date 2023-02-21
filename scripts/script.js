713cf6b117cc4d588a42dad1b070ca57645a1f5b
IGo <igor.smirna@gmail.com>
2023-02-21 16:48:44 +0300

    ПР5 рефак код, отпр на 1 review

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,7 @@ function createCard(name, link) {
 }
 
 // подгружаем данные для отк картинки на весь экран
-const popupImageDisplay = document.querySelector(".popup__image");
+const popupImageDisplay = document.querySelector(".popup__picture");
 function openPopupImage(e) {
   popupImageDisplay.querySelector(".popup__full").src = e.target.src;
   popupImageDisplay.querySelector(".popup__full").alt = e.target.alt;
